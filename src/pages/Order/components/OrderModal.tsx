e4d90ebcfc61f2072f0fde2c1b939456b1505d63
Huy <40709168+Cloneee@users.noreply.github.com>
2021-12-02 02:28:58 +0700

    Modal

--- src/pages/Order/components/OrderModal.tsx
+++ src/pages/Order/components/OrderModal.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
+import { Button, TextField } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+
+type Props = {
+  open: boolean;
+  handleAdd: (e: any) => void;
+  handleClose: (e: any) => void;
+  drink: any;
+};
 
 const style = {
   position: "absolute" as "absolute",
@@ -17,31 +25,32 @@ const style = {
   p: 4,
 };
 
-function OrderModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
+const OrderModal: React.FC<Props> = ({
+  open,
+  handleAdd,
+  handleClose,
+  drink,
+}) => {
   return (
-    <div>
-      <Button onClick={handleOpen}>Open modal</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h4" component="h2">
+          {drink.name || ''}
+        </Typography>
+        <Typography id="modal-modal-description" variant="h5" sx={{ mt: 2, float: 'right' }}>
+          {drink.price || ''} VND
+        </Typography>
+        <TextField type='number' id="standard-basic" label="Quantity" variant="standard" sx={{mt: 2}}/>
+        <TextField id="standard-basic" label="Description" variant="standard" sx={{mt: 2}}/>
+        <Button variant="contained" color="success" sx={{float: 'right'}} onClick={handleAdd}><AddIcon/></Button>
+      </Box>
+    </Modal>
   );
-}
+};
 
 export default OrderModal;
--- src/pages/Order/components/OrderTable.tsx
+++ src/pages/Order/components/OrderTable.tsx
@@ -1,13 +1,41 @@
-import React from "react";
-import { DataGrid, GridColDef, GridRowParams, GridValueGetterParams } from "@mui/x-data-grid";
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
+import { Box } from "@mui/material";
+import OrderModal from "./OrderModal";
+
+type Drinks = {
+  name: string;
+  price: number;
+};
 
 function OderTable() {
+  const [open, setOpen] = useState(false);
+  const [drink, setDrink] = useState<Drinks>({name: 'null', price: 0});
+  const [order, setOrder] = useState<Drinks[]>([]);
+
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "name", headerName: "Drink name", sortable: true, width: 360 },
-    { field: "category", headerName: "Category", sortable: true, width: 200 },
-    { field: "price", headerName: "Price", type: 'number', sortable: true, width: 200}
+    { field: "id", headerName: "ID", minWidth: 70 },
+    {
+      field: "name",
+      headerName: "Drink name",
+      sortable: true,
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "category",
+      headerName: "Category",
+      sortable: true,
+      minWidth: 150,
+      flex: 0.3,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      type: "number",
+      sortable: true,
+      minWidth: 100,
+    },
   ];
 
   const rows = [
@@ -27,21 +55,44 @@ function OderTable() {
     { id: 1, name: "Black Coffee", category: "Coffee", price: 20000 },
     { id: 1, name: "Black Coffee", category: "Coffee", price: 20000 },
     { id: 1, name: "Black Coffee", category: "Coffee", price: 20000 },
-    { id: 1, name: "Black Coffee", category: "Coffee", price: 20000 }
+    { id: 1, name: "Black Coffee", category: "Coffee", price: 20000 },
   ];
 
-  const handleClick = (e: GridRowParams) =>{
-    console.log(e)
-  }
+  const handleTableClick = (e: GridRowParams) => {
+    setOpen(true);
+    setDrink({name: e.row.name, price: e.row.price});
+  };
+
+  const handleAdd = (e: any) => {
+    if (order) {
+      setOrder([
+        ...order,
+        { name: drink.name, price: drink.price },
+      ]);
+    } else {
+      setOrder([{ name: drink.name, price: drink.price }]);
+    }
+    console.log(order)
+  };
+
+  const handleClose = (e: any) => {
+    setOpen(false);
+  };
 
   return (
-    <Box sx={{height: "80vh", width: "100%"}}>
+    <Box sx={{ height: "80vh", width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         autoPageSize
-        editMode={'row'}
-        onRowClick={handleClick}
+        editMode={"row"}
+        onRowClick={handleTableClick}
+      />
+      <OrderModal
+        open={open}
+        handleAdd={handleAdd}
+        handleClose={handleClose}
+        drink={drink}
       />
     </Box>
   );
