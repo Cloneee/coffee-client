c059ef5e31799667003d5ead8dec67e3ea669a77
Huy <40709168+Cloneee@users.noreply.github.com>
2021-12-04 01:50:03 +0700

    Change Reciption to Drawer

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -4,10 +4,12 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import {
   Box,
   Button,
+  Drawer,
   Grid,
   IconButton,
   List,
   ListItem,
+  styled,
   TextField,
   Typography,
 } from "@mui/material";
@@ -19,14 +21,40 @@ type Drinks = {
   name: string;
   price: number;
   quantity: number;
-  note?: string
+  note?: string;
 };
+
+const drawerWidth = 400;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginRight: -drawerWidth,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  }),
+}));
 
 const Order = (props: any) => {
   const [open, setOpen] = useState(false);
-  const [drink, setDrink] = useState<Drinks>({ name: "null", price: 0, quantity: 1 });
+  const [drink, setDrink] = useState<Drinks>({
+    name: "null",
+    price: 0,
+    quantity: 1,
+  });
   const [order, setOrder] = useState<Drinks[]>([]);
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
+  const [note, setNote] = useState("");
+  const [drawer, setDrawer] = useState(true);
 
   const handleTableClick = (e: GridRowParams) => {
     setOpen(true);
@@ -35,43 +63,64 @@ const Order = (props: any) => {
   const handleAdd = (e: React.MouseEvent<HTMLElement>) => {
     if (order) {
       const nameDrinkArr = order.map((item) => item.name);
-      const isDup = nameDrinkArr.some(
-        (item, idx) => {
-          return nameDrinkArr.indexOf(drink.name) === idx}
-      );
-      if(isDup){
-        const drinkIdx = nameDrinkArr.indexOf(drink.name)
-        order[drinkIdx].quantity +=  quantity
-      }
-      else{
-        setOrder([...order, { name: drink.name, price: drink.price, quantity: quantity }]);
+      const isDup = nameDrinkArr.some((item, idx) => {
+        return nameDrinkArr.indexOf(drink.name) === idx;
+      });
+      if (isDup) {
+        const drinkIdx = nameDrinkArr.indexOf(drink.name);
+        order[drinkIdx].quantity += quantity;
+      } else {
+        setOrder([
+          ...order,
+          {
+            name: drink.name,
+            price: drink.price,
+            quantity: quantity,
+            note: note,
+          },
+        ]);
       }
     } else {
-      setOrder([{ name: drink.name, price: drink.price, quantity: quantity }]);
+      setOrder([
+        {
+          name: drink.name,
+          price: drink.price,
+          quantity: quantity,
+          note: note,
+        },
+      ]);
     }
-    setOpen(false)
-    setQuantity(1)
+    setOpen(false);
+    setQuantity(1);
+    setNote("");
   };
   const handleClose = (e: React.MouseEvent<HTMLElement>) => {
     setOpen(false);
-    setQuantity(1)
+    setQuantity(1);
+    setNote("");
   };
   const handleThanhToan = (e: React.MouseEvent<HTMLElement>) => {
     console.log(order);
   };
-  
-  const handleChangeQuantity = (e: any)=>{
-    setQuantity(parseInt(e.nativeEvent.srcElement.value))
-    console.log(quantity);
-  }
 
-  const handleDelete = (e: React.MouseEvent<HTMLElement>) =>{
-    console.log(e)
-  }
+  const handleChangeQuantity = (e: any) => {
+    setQuantity(parseInt(e.nativeEvent.srcElement.value));
+  };
+  const handleChangeNote = (e: any) => {
+    setNote(e.nativeEvent.srcElement.value);
+  };
+
+  const handleDelete = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(e);
+  };
+
+  const toggleBill = (e: React.MouseEvent<HTMLElement>) => {
+    setDrawer(!drawer);
+  };
 
   return (
-    <Grid container sx={{ height: "100%" }}>
-      <Grid item xs={9} sx={{ height: "100%" }}>
+    <Box sx={{ height: "100%", display: "flex" }}>
+      <Main open={drawer}>
         <Grid
           container
           sx={{ bgcolor: "white", display: "flex", height: "100%" }}
@@ -95,11 +144,11 @@ const Order = (props: any) => {
             <OderTable
               open={open}
               drink={drink}
-              order={order}
               handleTableClick={handleTableClick}
               handleAdd={handleAdd}
               handleClose={handleClose}
               handleChangeQuantity={handleChangeQuantity}
+              handleChangeNote={handleChangeNote}
             ></OderTable>
           </Grid>
           <Grid item xs={12} sx={{ px: 1, pb: 1 }}>
@@ -111,7 +160,11 @@ const Order = (props: any) => {
             >
               Thanh toán
             </Button>
-            <Button variant="contained" sx={{ float: "right" }}>
+            <Button
+              variant="contained"
+              onClick={toggleBill}
+              sx={{ float: "right" }}
+            >
               Xem bill
             </Button>
             <Button variant="contained" color="error" sx={{ float: "left" }}>
@@ -119,33 +172,92 @@ const Order = (props: any) => {
             </Button>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={3}>
-        <Box sx={{ height: "100%", bgcolor: "white" }}>
-            <Typography align="center" variant="h3">Order's bill</Typography>
-          <Box sx={{ display: "flex" }}>
-            <List sx={{width: '100%'}}>
-              {order.map((item) => (
-                <ListItem
-                  key={item.name}
-                  secondaryAction={
-                    <IconButton id={item.name} edge="end" aria-label="delete" onClick={handleDelete}>
-                      <DeleteIcon color="primary"/>
-                    </IconButton>
-                  }
+      </Main>
+      <Drawer
+        anchor="right"
+        variant="persistent"
+        open={drawer}
+        onClose={toggleBill}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+          display: "flex",
+        }}
+      >
+        <Typography align="center" variant="h3" sx={{ pt: 8 }}>
+          Receiption
+        </Typography>
+        <Box
+          sx={{
+            flexGrow: 1,
+            borderTop: "1px solid gray",
+            borderBottom: "1px solid gray",
+          }}
+        >
+          <List sx={{ width: "100%" }}>
+            {order.map((item) => (
+              <ListItem
+                key={item.name}
+                secondaryAction={
+                  <IconButton
+                    id={item.name}
+                    edge="end"
+                    aria-label="delete"
+                    onClick={handleDelete}
+                  >
+                    <DeleteIcon color="primary" />
+                  </IconButton>
+                }
+                sx={{ display: "flex", flexDirection: "column" }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexGrow: 1,
+                    width: "100%",
+                  }}
                 >
-                  <Box sx={{display: 'flex', justifyContent: 'space-between', flexGrow: 1}}>
-                    <Typography sx={{flex: 1, pl: 1}}>{item.name}</Typography>
-                    <Typography align='left' sx={{minWidth: '30px'}}>x{item.quantity}</Typography>
-                    <Typography align='right' sx={{ml: 2, minWidth: '120px'}}>{(item.price * item.quantity).toLocaleString(undefined)} VND</Typography>
+                  <Typography sx={{ flex: 1, pl: 1 }}>{item.name}</Typography>
+                  <Typography align="left" sx={{ minWidth: "30px" }}>
+                    x{item.quantity}
+                  </Typography>
+                  <Typography align="right" sx={{ ml: 2, minWidth: "120px" }}>
+                    {(item.price * item.quantity).toLocaleString(undefined)} VND
+                  </Typography>
+                </Box>
+                {item.note ? (
+                  <Box sx={{ flexGrow: 1, width: "100%" }}>
+                    <Typography align="right" variant="subtitle2">
+                      - {item.note} -
+                    </Typography>
                   </Box>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
+                ) : (
+                  ""
+                )}
+              </ListItem>
+            ))}
+          </List>
         </Box>
-      </Grid>
-    </Grid>
+        <Box
+          sx={{
+            display: "flex",
+            p: 2,
+          }}
+        >
+          <Typography>Tổng</Typography>
+          <Typography align="right" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
+            {order
+              .map((el) => el.price * el.quantity)
+              .reduce((partial_sum, a) => partial_sum + a, 0)
+              .toLocaleString(undefined)}
+            VND
+          </Typography>
+        </Box>
+      </Drawer>
+    </Box>
   );
 };
 
