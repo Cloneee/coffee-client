58fc8793404c0e56e10a0b4d1596379a5a02aa6d
Huy <40709168+Cloneee@users.noreply.github.com>
2021-12-03 18:36:59 +0700

    Edit modal

--- src/pages/Order/components/OrderModal.tsx
+++ src/pages/Order/components/OrderModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
@@ -20,9 +20,9 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+  borderRadius: 5
 };
 
 const OrderModal: React.FC<Props> = ({
@@ -31,6 +31,11 @@ const OrderModal: React.FC<Props> = ({
   handleClose,
   drink,
 }) => {
+  const [quantity, setQuantity] = useState(1)
+  const handleChange = (e: any)=>{
+    setQuantity(parseInt(e.nativeEvent.srcElement.value))
+    console.log(quantity);
+  }
   return (
     <Modal
       open={open}
@@ -45,8 +50,8 @@ const OrderModal: React.FC<Props> = ({
         <Typography id="modal-modal-description" variant="h5" sx={{ mt: 2, float: 'right' }}>
           {drink.price || ''} VND
         </Typography>
-        <TextField type='number' id="standard-basic" label="Quantity" variant="standard" sx={{mt: 2}}/>
-        <TextField id="standard-basic" label="Description" variant="standard" sx={{mt: 2}}/>
+        <TextField type='number' id="standard-basic" label="Quantity" variant="standard" defaultValue={1} onChange={handleChange} sx={{mt: 2}}/>
+        <TextField id="standard-basic" label="Note" variant="standard" sx={{mt: 2}}/>
         <Button variant="contained" color="success" sx={{float: 'right'}} onClick={handleAdd}><AddIcon/></Button>
       </Box>
     </Modal>
